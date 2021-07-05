b0ca2b04d895646c1d287fd697f046bb611dd52d
dongjji <y2kdj9723@naver.com>
2021-07-06 01:31:17 +0900

    Day35 Filtering from Category with ejs

--- mongoAndExpress/index.js
+++ mongoAndExpress/index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 
 const Product = require("./models/product");
-
+const categories = ["fruit", "vegetable", "dairy"];
 mongoose
   .connect("mongodb://localhost:27017/farmStand", {
     useNewUrlParser: true,
@@ -29,47 +29,50 @@ app.use(methodOverride("_method"));
 // });
 
 app.get("/products", async (req, res) => {
-  const products = await Product.find({});
-  console.log(products);
-  res.render("products/index", { products });
+  const { category } = req.query;
+  if (category) {
+    const products = await Product.find({ category });
+    res.render("products/index", { products, category });
+  } else {
+    const products = await Product.find({});
+    res.render("products/index", { products, category: "All" });
+  }
 });
 
 app.get("/products/new", (req, res) => {
-  res.render("products/new");
+  res.render("products/new", { categories });
 });
 app.get("/products/:id", async (req, res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
-  console.log(product);
   res.render("products/show", { product });
   // Product.find({id:_id})
 });
 app.post("/products", async (req, res) => {
   const newProduct = new Product(req.body);
   await newProduct.save();
-  console.log(newProduct);
-  res.redirect("products");
+  res.redirect(`/products`);
 });
 app.get("/products/:id/edit", async (req, res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
-  res.render("products/edit", { product });
+  res.render("products/edit", { product, categories });
 });
 app.put("/products/:id", async (req, res) => {
   const { id } = req.params;
-  const product = Product.findByIdAndUpdate(id, req.body, {
+  const product = await Product.findByIdAndUpdate(id, req.body, {
     runValidators: true,
     new: true,
   });
   res.redirect(`/products/${product._id}`);
 });
-app.delete("/products/:id", (req, res) => {
-  const deleteProduct = new Product(req.body);
+app.delete("/products/:id", async (req, res) => {
   const { id } = req.params;
-  deleteProduct.findOneAndDelete({ id: `${id}` });
-  deleteProduct.save();
-  res.redirect("products");
+  const deleteProduct = await Product.findByIdAndDelete(id);
+  res.redirect("/products");
 });
+// delete를 할 때는 따로 ejs 창이 없기 때문에 app.delete를 실행하려면 delete form과 delete button을 만들어야함
+// anchor tag를 사용하면 작동하지 않는다.
 app.listen(3000, () => {
   console.log("App is Listening on port 3000");
 });
