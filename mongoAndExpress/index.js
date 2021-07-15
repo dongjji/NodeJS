b2cf91fa2f1ecc38f7df6227f0fe54e967576014
dongjji <y2kdj9723@naver.com>
2021-07-16 03:18:13 +0900

    Day45 deletion Middleware between schema relationship

--- mongoAndExpress/index.js
+++ mongoAndExpress/index.js
@@ -46,7 +46,7 @@ app.get("/products/new", (req, res) => {
 
 app.get("/products/:id", async (req, res) => {
   const { id } = req.params;
-  const product = await Product.findById(id);
+  const product = await Product.findById(id).populate("farm");
   res.render("products/show", { product });
   // Product.find({id:_id})
 });
@@ -99,8 +99,30 @@ app.post("/farms", async (req, res) => {
 
 app.get("/farms/:id", async (req, res) => {
   const { id } = req.params;
-  const farm = await Farm.findById(id);
+  const farm = await Farm.findById(id).populate("products");
   res.render("farms/show", { farm });
+});
+
+app.get("/farms/:id", async (req, res) => {
+  const farm = await Farm.findByIdAndDelete(req.params.id);
+  res.redirect("/farms");
+});
+
+app.get("/farms/:id/products/new", async (req, res) => {
+  const { id } = req.params;
+  const farm = await Farm.findById(id);
+  res.render("products/new", { categories, farm });
+});
+
+app.post("/farms/:id/products", async (req, res) => {
+  const { id } = req.params;
+  const farm = await Farm.findById(id);
+  const product = new Product(req.body);
+  farm.products.push(product);
+  product.farm = farm;
+  await farm.save();
+  await product.save();
+  res.redirect(`/farms/${id}`);
 });
 
 app.get("/farms/:id/edit", async (req, res) => {
--- mongoAndExpress/models/farm.js
+++ mongoAndExpress/models/farm.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Product = require("./product");
 const { Schema } = mongoose;
 
 const farmSchema = Schema({
@@ -14,6 +15,18 @@ const farmSchema = Schema({
     required: [true, "Email Required!"],
   },
   products: [{ type: Schema.Types.ObjectId, ref: "Product" }],
+});
+
+farmSchema.pre("findOneAndDelete", async (data) => {
+  console.log("pre middleware");
+  console.log(data);
+});
+
+farmSchema.post("findOneAndDelete", async (farm) => {
+  if (farm.products.length) {
+    const res = await Product.deleteMany({ _id: { $in: farm.products } });
+    console.log(res);
+  }
 });
 
 const Farm = mongoose.model("Farm", farmSchema);
