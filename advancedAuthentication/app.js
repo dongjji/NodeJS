b0089b965e711adf03d1aca2358e50a41f83b469
dongjji <y2kdj9723@naver.com>
2021-09-18 19:02:53 +0900

    Day109 advancedAuthentication(sendgrid,nodemailer)

---
+++ advancedAuthentication/app.js
@@ -0,0 +1,84 @@
+require("dotenv").config();
+const path = require("path");
+
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
+const flash = require("connect-flash");
+
+const errorController = require("./controllers/error");
+const User = require("./models/user");
+
+const mongodbURI = `mongodb+srv://dongjoon:${process.env.mongoConnectPassword}@cluster0.u6gwl.mongodb.net/shop?retryWrites=true&w=majority`;
+
+const app = express();
+const store = new MongoDBStore({
+  uri: mongodbURI,
+  collection: "sessions",
+});
+const csrfProtection = csrf();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "my secret is dongjoon",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+// csrfProtection after session
+app.use(csrfProtection);
+app.use(flash());
+
+app.use(async (req, res, next) => {
+  try {
+    if (req.session.user) {
+      await User.findById(req.session.user._id).then((user) => {
+        isAuthenticated = true;
+        req.user = user;
+      });
+    } else {
+      isAuthenticated = false;
+    }
+    next();
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.use(async (req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(mongodbURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    console.log("dbs connected");
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
